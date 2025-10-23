d480bc3e7c4cc66abcb5f3aba25edbf8b6490ec3
fdhhhdjd <nguyentientai10@gmail.com>
2025-10-23 11:42:15 +0700

    fix: zalo pay

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.post("/status", async (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`✅ Server chạy tại: http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
